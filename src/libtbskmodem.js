5d252df32156dff31ec22c0f3bb6153341fd402e
nyatla <nyatla39@gmail.com>
2023-01-21 12:49:51 +0900

    TBSKmodemCPPの更新

--- src/libtbskmodem.js
+++ src/libtbskmodem.js
@@ -7,7 +7,7 @@ import { Buffer } from 'buffer';
  * ref::
  * https://zenn.dev/wok/articles/0022_bundle-wasm
  */
-const VERSION="TBSKmodemJS/0.1.3";
+const VERSION="TBSKmodemJS/0.1.4";
 
 export const TBSKmodemJS={
     _instance:null,
